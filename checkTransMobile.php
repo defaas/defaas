6745382581b8b0714a1eccea78a96813656cfd79
defaas <79768987+defaas@users.noreply.github.com>
2021-03-02 05:50:15 +0700

    update index

--- checkTransMobile.php
+++ checkTransMobile.php
@@ -9,6 +9,45 @@
     //$merchantOrderId = "2FFE8AD44F41";
     $signature = md5($merchantCode . $merchantOrderId . $merchantKey);
 
+    
+    $itemsParam = array(
+        'merchantCode' => $merchantCode,
+        'signature' => $signature
+    );
+    
+    class emp{}
+    
+    $params = array_merge((array)$result,$itemsParam);
+    $params_string = json_encode($params);
+    
+    //if sandbox
+    $url = 'http://sandbox.duitku.com/webapi/api/merchant/transactionStatus';
+    //if production
+    //$url = 'https://passport.duitku.com/webapi/api/merchant/transactionStatus';
+    
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);                                                                  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($params_string))                                                                       
+    );   
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    //execute post
+    $request = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if($httpCode == 200)
+    {
+	      echo $request;
+    }
+    else
+        echo $httpCode;
+/* 
     $params = array(
         'merchantCode' => $merchantCode,
         'merchantOrderId' => $merchantOrderId,
@@ -39,5 +78,5 @@
         //var_dump($result);
     }
     else
-        echo $httpCode;
+        echo $httpCode; */
 ?>
