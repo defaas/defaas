0f6ef9835fb0ce073efdcb2460f244c559f0efb9
defaas <79768987+defaas@users.noreply.github.com>
2021-03-02 05:41:09 +0700

    update index

--- checkTransMobile.php
+++ checkTransMobile.php
@@ -5,7 +5,7 @@
     
     $merchantCode = "D7993"; // from duitku
     $merchantKey = "096004ad69feb734317b50d4d6565ff0"; // from duitku
-    $merchantOrderId = $result->{'merchantOrderId'}; // from merchant, unique
+    $merchantOrderId = $result->{'reference'}; // from merchant, unique
     //$merchantOrderId = "2FFE8AD44F41";
     $signature = md5($merchantCode . $merchantOrderId . $merchantKey);
 
