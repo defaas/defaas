c10493cadbcb64c531ea30ebec7cd16db41ecfb8
defaas <79768987+defaas@users.noreply.github.com>
2021-03-02 04:06:01 +0700

    update index

---
+++ checkTransMobile.php
@@ -0,0 +1,38 @@
+<?php
+    $merchantCode = "D7993"; // from duitku
+    $merchantKey = "096004ad69feb734317b50d4d6565ff0"; // from duitku
+    $merchantOrderId = $result->{'merchantOrderId'}; // from merchant, unique
+
+    $signature = md5($merchantCode . $merchantOrderId . $merchantKey);
+
+    $params = array(
+        'merchantCode' => $merchantCode,
+        'merchantOrderId' => $merchantOrderId,
+        'signature' => $signature
+    );
+
+    $params_string = json_encode($params);
+    $url = 'http://sandbox.duitku.com/webapi/api/merchant/transactionStatus';
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);                                                                  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($params_string))                                                                       
+    );   
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    //execute post
+    $request = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if($httpCode == 200)
+    {
+        $result = json_decode($request, true);
+    }
+    else
+        echo $httpCode;
+?>
