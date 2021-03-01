80673de7d783a2a91655a7c9d8490618e0d1a47a
defaas <79768987+defaas@users.noreply.github.com>
2021-03-02 03:11:02 +0700

    update index

---
+++ reqTrans.php
@@ -0,0 +1,116 @@
+<?php
+    $merchantCode = "D7993"; // from duitku
+    $merchantKey = "096004ad69feb734317b50d4d6565ff0"; // from duitku
+    $paymentAmount = 40000;
+    $paymentMethod = 'VC'; // VC = Credit Card
+    $merchantOrderId = "1234567890"; // from merchant, unique
+    $productDetails = 'Test Pay with duitku';
+    $email = 'test@test.com'; // your customer email
+    $phoneNumber = '08123456789'; // your customer phone number (optional)
+    $additionalParam = ''; // optional
+    $merchantUserInfo = ''; // optional
+    $customerVaName = 'John Doe'; // display name on bank confirmation display
+    $callbackUrl = "http://defaas.herokuapp.com/callBackUrl.php"; // url for callback
+    $returnUrl = "http://defaas.herokuapp.com/returnUrl.php"; // url for redirect
+    $expiryPeriod = 10; // set the expired time in minutes
+    $signature = md5($merchantCode . $merchantOrderId . $paymentAmount . $merchantKey);
+
+    // Customer Detail
+    $firstName = "John";
+    $lastName = "Doe";
+
+    // Address
+    $alamat = "Jl. Kembangan Raya";
+    $city = "Jakarta";
+    $postalCode = "11530";
+    $countryCode = "ID";
+
+    $address = array(
+        'firstName' => $firstName,
+        'lastName' => $lastName,
+        'address' => $alamat,
+        'city' => $city,
+        'postalCode' => $postalCode,
+        'phone' => $phoneNumber,
+        'countryCode' => $countryCode
+    );
+
+    $customerDetail = array(
+        'firstName' => $firstName,
+        'lastName' => $lastName,
+        'email' => $email,
+        'phoneNumber' => $phoneNumber,
+        'billingAddress' => $address,
+        'shippingAddress' => $address
+    );
+
+
+    $item1 = array(
+        'name' => 'Test Item 1',
+        'price' => 10000,
+        'quantity' => 1);
+
+    $item2 = array(
+        'name' => 'Test Item 2',
+        'price' => 30000,
+        'quantity' => 3);
+
+    $itemDetails = array(
+        $item1, $item2
+    );
+
+    $params = array(
+        'merchantCode' => $merchantCode,
+        'paymentAmount' => $paymentAmount,
+        'paymentMethod' => $paymentMethod,
+        'merchantOrderId' => $merchantOrderId,
+        'productDetails' => $productDetails,
+        'additionalParam' => $additionalParam,
+        'merchantUserInfo' => $merchantUserInfo,
+        'customerVaName' => $customerVaName,
+        'email' => $email,
+        'phoneNumber' => $phoneNumber,
+        'itemDetails' => $itemDetails,
+        'customerDetail' => $customerDetail,
+        'callbackUrl' => $callbackUrl,
+        'returnUrl' => $returnUrl,
+        'signature' => $signature,
+        'expiryPeriod' => $expiryPeriod
+
+    );
+
+    $params_string = json_encode($params);
+    //echo $params_string;
+    $url = 'https://sandbox.duitku.com/webapi/api/merchant/v2/inquiry'; // Sandbox
+    // $url = 'https://passport.duitku.com/webapi/api/merchant/v2/inquiry'; // Production
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);                                                                  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($params_string))                                                                       
+    );   
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    //execute post
+    $request = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if($httpCode == 200)
+    {
+        $result = json_decode($request, true);
+        //header('location: '. $result['paymentUrl']);
+        echo "paymentUrl :". $result['paymentUrl'] . "<br />";
+        echo "merchantCode :". $result['merchantCode'] . "<br />";
+        echo "reference :". $result['reference'] . "<br />";
+        echo "vaNumber :". $result['vaNumber'] . "<br />";
+        echo "amount :". $result['amount'] . "<br />";
+        echo "statusCode :". $result['statusCode'] . "<br />";
+        echo "statusMessage :". $result['statusMessage'] . "<br />";
+    }
+    else
+        echo $httpCode;
+?>
